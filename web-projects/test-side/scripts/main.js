dd55018f6584bd43990c23992ef1f5409f8df461
welschoff <90267486+welschoff@users.noreply.github.com>
2021-09-07 16:49:30 +0200

    Add files via upload

---
+++ web-projects/test-side/scripts/main.js
@@ -0,0 +1,38 @@
+
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'test-side/images/bvb.png') {
+      myImage.setAttribute('src','test-side/images/bvb2.png');
+    } else {
+      myImage.setAttribute('src','test-side/images/bvb.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'BORUSSIA DORTMUND, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'BORUSSIA DORTMUND, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+  
+  
